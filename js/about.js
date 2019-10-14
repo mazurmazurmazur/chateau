a544d64347a98e9a3784e114429ca9d531ab9550
mazurjestboski <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2019-10-14 16:48:07 +0200

    new about adjusted for chinese

--- js/about.js
+++ js/about.js
@@ -9,7 +9,9 @@ window.onscroll = function() {
     navBg.style.top = '-20vh';
     langSelected.style.color = 'white';
     setTimeout(function() {
-      mainLogo.src = '../images/logowhitecn.png';
+      if (document.documentElement.lang == 'cn')
+        mainLogo.src = '../images/logowhitecn.png';
+      else mainLogo.src = '../images/logo_white.png';
       globe.src = '../images/globebw.png';
     }, 300);
 
@@ -20,7 +22,10 @@ window.onscroll = function() {
     navBg.style.top = '0';
     langSelected.style.color = 'black';
     setTimeout(function() {
-      mainLogo.src = '../images/logoMobileCN.png';
+      if (document.documentElement.lang == 'cn')
+        mainLogo.src = '../images/logoMobileCN.png';
+      else mainLogo.src = '../images/logo_black.png';
+
       globe.src = '../images/globe.png';
     }, 300);
     burgerIcon.forEach(function(item) {
