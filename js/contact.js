e2dc1413eea31e35ae11e6a30d7465a19177bf9c
mazurjestboski <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2019-10-31 14:10:42 +0100

    contact modal

--- js/contact.js
+++ js/contact.js
@@ -109,3 +109,72 @@ var greenIcon = L.icon({
 });
 
 var marker = L.marker([46.322, 29.29], { icon: greenIcon }).addTo(map);
+
+// Make the DIV element draggable:
+dragElement(document.querySelector('.modal'));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + 'header')) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+    elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close-modal')[0];
+
+// When the user clicks on the button, open the modal
+$(window).on('load', function() {
+  if (window.matchMedia('screen and (min-width: 761px)').matches) {
+    setTimeout(function() {
+      modal.style.display = 'block';
+    }, 4000);
+  }
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+};
